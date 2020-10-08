97fcbbe44e3b2ed6b40442a4a8b09c14494043b8
adeoluwadavid <adeoluwadavid@gmail.com>
2020-10-08 02:19:50 +0100

    AA

--- src/main/java/com/example/project/model/User.java
+++ src/main/java/com/example/project/model/User.java
@@ -17,8 +17,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
+
 
 @Entity(name="User")
 @Table(name="user")
